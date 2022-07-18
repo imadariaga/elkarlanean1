d06d147220312ab9394667644ffa20cbe4cec37e
Idoia Portatiletik <imadariaga@uni.eus>
2022-07-18 18:37:19 +0200

    Lehen bertsioa

---
+++ Aurkezpenak.java
@@ -0,0 +1,15 @@
+public class Aurkezpenak{
+   
+    /** *main* metodoa da Java programa baten sarrera puntua.
+     * Goiburukoa beti berdina izango da.
+     * Gure "Aurkezpenak" izeneko programa honek ez du argumenturik jasoko
+     * Elkarlanean osatuko dugu iturburuko kodea. 
+     * 
+     */
+    public static void main(String[] args) {
+
+        System.out.println("00 => Kaixo. Idoia Madariaga naiz. Java lengoaian programatzen lagunduko dizuet. Urte asko daramatzat programatzen. (Ez dizuet esango zenbat ;-) )");
+        System.out.println("01 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("02 => ... naiz. ... tik nator. Iaz...");
+    }
+}
