4f917c086e1ece5de55511114607f5d29884faa2
Idoia Portatiletik <imadariaga@uni.eus>
2022-07-19 08:33:28 +0200

    Lerro bat ikasleko gehituta

--- Aurkezpenak.java
+++ Aurkezpenak.java
@@ -7,9 +7,33 @@ public class Aurkezpenak{
      * 
      */
     public static void main(String[] args) {
-
+        System.out.println("GELAKIDEEN AURKEZPENAK:");
+        System.out.println("=======================");
         System.out.println("00 => Kaixo. Idoia Madariaga naiz. Java lengoaian programatzen lagunduko dizuet. Urte asko daramatzat programatzen. (Ez dizuet esango zenbat ;-) )");
         System.out.println("01 => ... naiz. ... tik nator. Iaz...");
-        System.out.println("02 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("02 => ... naiz. ... koa naiz. ... ikasi nuen.");
+        System.out.println("03 => ... naiz. ... tik nator. Denbora librean... ");
+        System.out.println("04 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("05 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("06 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("07 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("08 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("09 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("10 => ... naiz. ... koa naiz. ... ikasi nuen.");
+        System.out.println("11 => ... naiz. ... tik nator. Denbora librean... ");
+        System.out.println("12 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("13 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("14 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("15 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("16 => ... naiz. ... tik nator. Iaz...");   
+        System.out.println("17 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("18> ... naiz. ... koa naiz. ... ikasi nuen.");
+        System.out.println("19 => ... naiz. ... tik nator. Denbora librean... ");
+        System.out.println("20 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("21 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("22 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("23 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("24 => ... naiz. ... tik nator. Iaz...");
+        System.out.println("25 => ... naiz. ... tik nator. Iaz...");
     }
 }
